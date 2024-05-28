7c283563c3126e5613d8bf57e7d42bc7fe48f4f0
sparqxi <davidkim052894@gmail.com>
2024-05-28 02:19:41 -0700

    token address env

--- src/router/update.rs
+++ src/router/update.rs
@@ -1,69 +1,75 @@
+use std::ops::{Div, Sub};
+
 use actix_web::{
     get,
     web::{self, Path},
     HttpResponse,
 };
+use solana_transaction_status::{option_serializer::OptionSerializer, EncodedTransaction, UiTransaction};
 
-use crate::{service::db::Database, utils::transaction::get_data_from_transaction};
+use crate::{service::db::Database, utils::transaction::{extract_token_balances, get_data_from_transaction}};
 
 #[get("/claimed/{tx}")]
 async fn claimed(db: web::Data<Database>, path: Path<String>) -> HttpResponse {
     let signature = path.into_inner();
     println!("sig={signature}");
+    let token_mint_address =  dotenv::var("TOKEN_MINT_ADDRESS").unwrap();
 
     let transaction = get_data_from_transaction(signature).await;
 
     match transaction {
         Ok(tr) => match tr {
             Ok(t) => {
-                // println!("t=={:?}",t);
                 let claimed_time = t.block_time.unwrap();
 
                 let s = t.transaction.meta;
+                let sign = t.transaction.transaction;
+                let accounts;
+               let signer = match sign {
+                    EncodedTransaction::LegacyBinary(_) => todo!(),
+                    EncodedTransaction::Binary(_, _) => todo!(),
+                    EncodedTransaction::Json(sg) => {
+                      
+                       match sg.message {
+                           solana_transaction_status::UiMessage::Parsed(_) => todo!(),
+                           solana_transaction_status::UiMessage::Raw(h) => {
+                            accounts = h;
+                            accounts.account_keys.first().unwrap()
+                           },
+                       }
+                       
+                    },
+                    EncodedTransaction::Accounts(_) => todo!(),
+                };
+                    
+                
                 match s {
                     Some(s) => {
                         let pre_token = s.pre_token_balances;
                         let post_token = s.post_token_balances;
                       let pre =  match pre_token {
-                            solana_transaction_status::option_serializer::OptionSerializer::Some(s) => {
-                               match s.last() {
-                                   Some(s) => {
-                                    // println!("uitoken={}",s.ui_token_amount.ui_amount.unwrap());
-                                    s.ui_token_amount.ui_amount.unwrap()
-                                   },
-                                   None => {0.0},
-                               }
+                            OptionSerializer::Some(s) => {
+                              extract_token_balances(s, token_mint_address.clone(), signer.to_string())
 
                                 // HttpResponse::Ok().body("success")
                             },
-                            solana_transaction_status::option_serializer::OptionSerializer::None => todo!(),
-                            solana_transaction_status::option_serializer::OptionSerializer::Skip => todo!(),
+                            OptionSerializer::None => todo!(),
+                            OptionSerializer::Skip => todo!(),
                         
                             
                         };
-                        let (owner, post) = match post_token {
-                            solana_transaction_status::option_serializer::OptionSerializer::Some(st) => {
-                                match st.last() {
-                                    Some(s) => {
-                                        // Clone the necessary data from `s` before the borrow ends.
-                                        let ui_token_amount = s.ui_token_amount.ui_amount.unwrap();
-                                        let owner = match &s.owner {
-                                            solana_transaction_status::option_serializer::OptionSerializer::Some(o) => o.clone(),
-                                            solana_transaction_status::option_serializer::OptionSerializer::None => todo!(),
-                                            solana_transaction_status::option_serializer::OptionSerializer::Skip => todo!(),
-                                        };
-                                        (owner, ui_token_amount)
-                                    },
-                                    None => todo!(),
-                                }
+                        let  post = match post_token {
+                            OptionSerializer::Some(st) => {
+                                extract_token_balances(st, token_mint_address.clone(), signer.to_string())
+
                             },
-                            solana_transaction_status::option_serializer::OptionSerializer::None => todo!(),
-                            solana_transaction_status::option_serializer::OptionSerializer::Skip => todo!(),
+                            OptionSerializer::None => todo!(),
+                            OptionSerializer::Skip => todo!(),
                         };
                         
                         let balance = post - pre;
-                        println!("token=={}",balance.abs());
-                        match db.update_beneficiary(owner.to_string(),claimed_time,balance).await {
+                        println!("token=={}",post.sub(pre));
+                        match db.update_beneficiary(signer.to_string(),claimed_time,post.sub(pre).div(1000000000.0)).await {
                             Ok(success) => {
                                 println!("success{:?}",success.unwrap());
                                 HttpResponse::Ok().body("success")
--- src/utils/transaction.rs
+++ src/utils/transaction.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 use actix_web::web;
 use solana_client::{rpc_client::RpcClient, rpc_config::RpcTransactionConfig};
 use solana_sdk::{commitment_config::CommitmentConfig, signature::Signature};
-use solana_transaction_status::{EncodedConfirmedTransactionWithStatusMeta, UiTransactionEncoding};
+use solana_transaction_status::{EncodedConfirmedTransactionWithStatusMeta, UiTransactionEncoding, UiTransactionTokenBalance};
 
 pub async fn get_data_from_transaction(signature:String) -> Result<Result<EncodedConfirmedTransactionWithStatusMeta, solana_client::client_error::ClientError>, actix_web::error::BlockingError> {
     let sign = Signature::from_str(&signature).unwrap();
@@ -18,4 +18,17 @@ pub async fn get_data_from_transaction(signature:String) -> Result<Result<Encode
         client.get_transaction_with_config(&sign, config)
     }).await
 
+}
+
+pub fn extract_token_balances(balances:Vec<UiTransactionTokenBalance>,token_mint_address:String,owner:String) ->f64 {
+    balances.iter().filter(|balance| {
+        let tx_owner = match &balance.owner {
+            solana_transaction_status::option_serializer::OptionSerializer::Some(e) => e,
+            solana_transaction_status::option_serializer::OptionSerializer::None => todo!(),
+            solana_transaction_status::option_serializer::OptionSerializer::Skip => todo!(),
+        };
+
+        return balance.mint == token_mint_address && *tx_owner == owner;
+
+    }).map(|balance| balance.ui_token_amount.amount.parse::<f64>().unwrap_or(0.0)).sum()
 }
